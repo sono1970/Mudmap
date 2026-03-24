e583e94a3c5dbb559f466054e0ddafc5c373e959
sono1970 <stevefel@gmail.com>
2026-03-24 13:17:02 +0000

    sw.js

--- Sw.js
+++ Sw.js
@@ -1,1 +1,15 @@
 
+const cacheName = 'mudmap-v1';
+const assets = [
+  './MudMap.html',
+  './manifest.json',
+  'https://cdnjs.cloudflare.com/ajax/libs/fabric.js/5.3.1/fabric.min.js'
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(cacheName).then(cache => cache.addAll(assets)));
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(caches.match(e.request).then(res => res || fetch(e.request)));
+});
